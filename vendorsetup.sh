c706c56a246cf73a14312df76d0bdf1496ab1499
deny <77781846+deny2311@users.noreply.github.com>
2024-10-22 15:33:55 +0700

    sagit: add vendorsetup.sh to sync with ksu

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+rm -rf kernel/xiaomi/msm8998
+git clone --recurse-submodules -b pixelage-su https://github.com/Sagit-A15/android_kernel_xiaomi_msm8998.git kernel/xiaomi/msm8998
+rm -rf system/extras/su
