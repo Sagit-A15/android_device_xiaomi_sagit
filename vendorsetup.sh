60a34f7f672997dc8b0a98269992b42831b35eeb
deny <77781846+deny2311@users.noreply.github.com>
2024-12-16 17:48:10 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
 rm -rf kernel/xiaomi/msm8998
 git clone --recurse-submodules -b lineage-22.0-su https://github.com/Sagit-A15/android_kernel_xiaomi_msm8998-cip.git kernel/xiaomi/msm8998
 rm -rf system/extras/su
+rm -rf vendor/pixelage
+git clone -b 15 https://github.com/Sagit-A15/android_vendor_pixelage.git vendor/pixelage
