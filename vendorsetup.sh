a200766a3a03d4127eae328f2476bff6e3900f90
deny <77781846+deny2311@users.noreply.github.com>
2024-12-21 15:53:19 +0700

    sagit: fix from source

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,3 @@
 rm -rf kernel/xiaomi/msm8998
 git clone --recurse-submodules -b lineage-22.0-su https://github.com/Sagit-A15/android_kernel_xiaomi_msm8998-cip.git kernel/xiaomi/msm8998
 rm -rf system/extras/su
-rm -rf hardware/lineage/interfaces
-git clone -b 15 https://github.com/Sagit-A15/android_hardware_lineage_interfaces.git hardware/lineage/interfaces
