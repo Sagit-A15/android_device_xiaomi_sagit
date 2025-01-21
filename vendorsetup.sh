0cf55c80b7379e693bb34c461f6b9abbe0ed9330
denysaja <143594138+denysaja@users.noreply.github.com>
2025-01-21 15:17:26 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,5 +2,6 @@ rm -rf kernel/xiaomi/msm8998
 git clone --recurse-submodules -b 15-next3 https://github.com/Sagit-A15/android_kernel_xiaomi_msm8998-cip.git kernel/xiaomi/msm8998
 cd kernel/xiaomi/msm8998
 rm -rf KernelSU-Next
-curl -kLSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash - 
+curl -kLSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
+cd ../../..
 rm -rf system/extras/su
