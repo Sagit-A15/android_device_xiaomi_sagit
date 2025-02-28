99f5ff1203b39e268a9ec6923fb008619f825c43
deny2311 <deny2311@gmail.com>
2025-02-28 15:09:20 +0000

    sagit: sync audio, display, media from los

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,9 @@
 rm -rf kernel/xiaomi/msm8998
 git clone --recurse-submodules -b lineage-22.0-su https://github.com/Sagit-A15/android_kernel_xiaomi_msm8998-cip.git kernel/xiaomi/msm8998
 rm -rf system/extras/su
+rm -rf hardware/qcom-caf/msm8998/audio
+rm -rf hardware/qcom-caf/msm8998/display
+rm -rf hardware/qcom-caf/msm8998/media
+git clone -b lineage-22.1-caf-msm8998 https://github.com/LineageOS/android_hardware_qcom_audio.git hardware/qcom-caf/msm8998/audio
+git clone -b lineage-22.1-caf-msm8998 https://github.com/LineageOS/android_hardware_qcom_display.git hardware/qcom-caf/msm8998/display
+git clone -b lineage-22.1-caf-msm8998 https://github.com/LineageOS/android_hardware_qcom_media.git hardware/qcom-caf/msm8998/media
