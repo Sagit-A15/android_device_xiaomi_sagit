f52664c0b655699f53167c135358c2777924db44
deny2311 <deny2311@gmail.com>
2025-04-16 07:32:32 +0000

    sagit: remove vendorsetup

--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-rm -rf kernel/xiaomi/msm8998
-git clone --recurse-submodules -b lineage-22.0-su https://github.com/Sagit-A15/android_kernel_xiaomi_msm8998-cip.git kernel/xiaomi/msm8998
-rm -rf system/extras/su
